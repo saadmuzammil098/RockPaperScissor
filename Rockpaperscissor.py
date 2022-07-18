e61df1939d9800f5eab01a6ec467984241a15152
Saad Muzammil <46246456+saadmuzammil098@users.noreply.github.com>
2022-07-18 13:50:40 +0500

    Add files via upload

---
+++ Rockpaperscissor.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  6 13:58:55 2022
+
+@author: SaadMuzammil
+"""
+
+import random
+def play():
+    user = input ("'r'  for rock, 'p' for paper, 's' for scissors: ")
+    computer = random.choice(['r', 'p', 's'])
+    
+    if user == computer:
+        return 'tie'
+    if is_win(user, computer):
+        return 'You Won'
+    
+    return 'You lost'
+
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+        or (player == 'p' and opponent == 'r'):
+        return True
+    
+
+print(play())
